73e2da2f376e0d9a326287abe0057e7d27ad24d2
azertui <rauch.arthur@gmail.com>
2019-12-22 02:16:39 +0100

    remove docs

--- Doxygen/html/search/variables_2.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['type',['type',['../structast.html#a8321fc8e556a418583dd74ee9fcc2bf7',1,'ast']]],
-  ['type_5fint',['type_int',['../structast.html#a48c1fea64ccd73c68d3901c23d65f9f5',1,'ast']]]
-];
